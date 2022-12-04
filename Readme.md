20504c96b8315793d06e5d06fbf8de6dfd160d0f
Steven Yu <steven@StevendeMacBook-Pro.local>
2022-12-04 21:51:08 +0800

    first commit

---
+++ Readme.md
@@ -0,0 +1,4 @@
+# OpenAI GPT-3 Telegram Chatbot
+![](img/1.png)
+
+![](img/2.png)
---
+++ main.py
@@ -0,0 +1,63 @@
+import openai
+from flask import Flask, request
+import telegram
+from telegram.ext import Dispatcher, MessageHandler, Filters
+import configparser
+
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+bot = telegram.Bot(token=(config['Telegram']['token']))
+app = Flask(__name__)
+
+
+@app.route('/hook', methods=['POST'])
+def webhook_handler():
+    if request.method == "POST":
+        update = telegram.Update.de_json(request.get_json(force=True), bot)
+        dispatcher.process_update(update)
+    return 'ok'
+
+
+openai.api_key = config['OPENAI']['key']
+
+
+def chat_ai(input_str):
+    response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt=f"Human: {input_str}",
+    temperature=0.9,
+    max_tokens=999,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0.6,
+    stop=[" Human:", " AI:"]
+    )
+
+    print(response['choices'])
+    res = response['choices'][0]['text']
+    if res.startswith("？") or res.startswith("?"):
+        res = res[1:]
+    return res.strip()
+
+
+def reply_handler(update ,bot):
+    """Reply message."""
+    try:
+        text = update.message.text
+        if text.startswith("機器人："):
+          text = text[4:]
+          print(text)
+          res = chat_ai(text)
+          print(res)
+          update.message.reply_text(res)
+
+    except Exception as e:
+        print(e)
+
+dispatcher = Dispatcher(bot, None)
+dispatcher.add_handler(MessageHandler(Filters.text, reply_handler))
+
+if __name__ == "__main__":
+    # Running server
+    app.run()
